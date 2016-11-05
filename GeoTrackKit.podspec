75d96a2467defd3c7c4144aa8d4a47dc8b81d4dc
Eric Internicola <intere@gmail.com>
2016-11-05 09:18:42 -0600

    Initial commit, to test, locally.

---
+++ GeoTrackKit.podspec
@@ -0,0 +1,16 @@
+# GeoTrackKit.podspec
+
+Pod::Spec.new do |s|
+  s.name      = 'GeoTrackKit'
+  s.version   = '0.0.1'
+  s.summary   = 'Geo Tracking and statistics for iOS'
+  s.author = {
+    'Eric Internicola' => 'intere@gmail.com'
+  }
+  s.source = {
+    :git => 'git@github.com:intere/GeoTrackKit.git',
+    :branch => 'master'
+    # :tag => '0.0.1'
+  }
+  s.source_files = 'GeoTrackKit/*.swift'
+end
---
+++ GeoTrackKitExample/GeoTrackKitExample/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GeoTrackKitExample
+//
+//  Created by Eric Internicola on 11/5/16.
+//  Copyright © 2016 Eric Internicola. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
