15c46e19883cf08afd9d6d786f5eafa46e2d659c
Eric Internicola <intere@gmail.com>
2017-08-18 21:22:59 -0600

    Updated the serialization tests.

--- GeoTrackKitExample/GeoTrackKitExampleTests/GeoTrackSerializationTests.swift
+++ GeoTrackKitExample/GeoTrackKitExampleTests/GeoTrackSerializationTests.swift
@@ -6,124 +6,112 @@
 //  Copyright © 2016 Eric Internicola. All rights reserved.
 //
 
-// TODO(EGI): Convert this back to XCTestCase
+import XCTest
+import GeoTrackKit
+import CoreLocation
 
-//import XCTest
-//import Quick
-//import Nimble
-//import GeoTrackKit
-//import CoreLocation
-//
-//class GeoTrackSerializationTests: QuickSpec {
-//
-//    override func spec() {
-//        describe("serialization") {
-//
-//            it("does serialize the track name") {
-//                let track = GeoTrack()
-//                track.name = "fake"
-//                expect(track.map["name"] as? String).to(equal("fake"))
-//            }
-//
-//            it("does serialize the track description") {
-//                let track = GeoTrack()
-//                track.description = "fake"
-//                expect(track.map["description"] as? String).to(equal("fake"))
-//            }
-//
-//            it("does serialize the track points") {
-//                let points = [
-//                    CLLocation(latitude: 1.234, longitude: 1.234),
-//                    CLLocation(latitude: 2.345, longitude: 2.345)
-//                ]
-//                let track = GeoTrack()
-//                points.forEach { track.add(location: $0) }
-//                guard let pointMaps = track.map["points"] as? [[String:Any]] else {
-//                    fail("Failed to get the right type of data from the map for the points")
-//                    return
-//                }
-//                expect(pointMaps.count).to(equal(2))
-//                expect(pointMaps.first?["lat"] as? CLLocationDegrees).to(equal(1.234))
-//                expect(pointMaps.first?["lon"] as? CLLocationDegrees).to(equal(1.234))
-//                expect(pointMaps.last?["lat"] as? CLLocationDegrees).to(equal(2.345))
-//                expect(pointMaps.last?["lon"] as? CLLocationDegrees).to(equal(2.345))
-//
-//            }
-//
-//            it("does serialze the track events") {
-//                let events = [
-//                    GeoTrackLocationEvent.startedTracking(message: "fake start"),
-//                    GeoTrackLocationEvent.error(message: "fake error"),
-//                    GeoTrackLocationEvent.custom(message: "fake custom")
-//                ]
-//                let track = GeoTrack()
-//                events.forEach { track.add(event: $0) }
-//                guard let eventMaps = track.map["events"] as? [[String:Any]] else {
-//                    fail("Failed to get the right type of data from the map for the events")
-//                    return
-//                }
-//                expect(eventMaps.count).to(equal(3))
-//                guard let first = eventMaps.first else {
-//                    fail("Failed to get the first event from the map array")
-//                    return
-//                }
-//                expect(first["type"] as? Int).to(equal(GeoTrackLocationEvent.EventType.startedTrack.rawValue))
-//                expect(first["timestamp"]).toNot(beNil())
-//                expect(first["message"] as? String).to(equal("fake start"))
-//
-//                let second = eventMaps[1]
-//                expect(second["type"] as? Int).to(equal(GeoTrackLocationEvent.EventType.error.rawValue))
-//                expect(second["timestamp"]).toNot(beNil())
-//                expect(second["message"] as? String).to(equal("fake error"))
-//
-//                guard let third = eventMaps.last else {
-//                    fail("Failed to get the third event from the map array")
-//                    return
-//                }
-//                expect(third["type"] as? Int).to(equal(GeoTrackLocationEvent.EventType.custom.rawValue))
-//                expect(third["timestamp"]).toNot(beNil())
-//                expect(third["message"] as? String).to(equal("fake custom"))
-//            }
-//        }
-//
-//        describe("Track File Reading Tests") {
-//
-//            it("does read a track file") {
-//                let reader = TrackReader(filename: "reference-track-1")
-//                expect(reader).toNot(beNil())
-//                expect(reader.track).toNot(beNil())
-//
-//                guard let points = reader.track?.points else {
-//                    fail("No points")
-//                    return
-//                }
-//
-//                expect(points.count).to(beGreaterThan(0))
-//
-//                var last: Date?
-//                for point in points {
-//                    defer {
-//                        last = point.timestamp
-//                    }
-//                    guard let lastTime = last else {
-//                        continue
-//                    }
-//                    expect(lastTime.timeIntervalSince1970).to(beLessThan(point.timestamp.timeIntervalSince1970))
-//                }
-//            }
-//
-//            it("does analyze a track file") {
-//                let reader = TrackReader(filename: "reference-track-1")
-//                guard let track = reader.track else {
-//                    fail("No track")
-//                    return
-//                }
-//                let analyzer = GeoTrackAnalyzer(track: track)
-//                analyzer.calculate()
-//            }
-//
-//        }
-//    }
-//
-//}
+class GeoTrackSerializationTests: XCTestCase {
 
+    func testSerializeTrackName() {
+        let track = GeoTrack()
+        track.name = "fake"
+        XCTAssertEqual(track.name, track.map["name"] as? String)
+    }
+
+    func testSerializeTrackDescription() {
+        let track = GeoTrack()
+        track.description = "fake"
+        XCTAssertEqual(track.description, track.map["description"] as? String)
+    }
+
+    func testSerializeTrackPoints() {
+        let points = [
+            CLLocation(latitude: 1.234, longitude: 1.234),
+            CLLocation(latitude: 2.345, longitude: 2.345)
+        ]
+        let track = GeoTrack()
+        points.forEach { track.add(location: $0) }
+        guard let pointMaps = track.map["points"] as? [[String:Any]] else {
+            return XCTFail("Failed to get the right type of data from the map for the points")
+        }
+
+        XCTAssertEqual(2, pointMaps.count)
+        XCTAssertEqual(points[0].coordinate.latitude, pointMaps.first?["lat"] as? CLLocationDegrees)
+        XCTAssertEqual(points[0].coordinate.longitude, pointMaps.first?["lon"] as? CLLocationDegrees)
+        XCTAssertEqual(points[1].coordinate.latitude, pointMaps.last?["lat"] as? CLLocationDegrees)
+        XCTAssertEqual(points[1].coordinate.longitude, pointMaps.last?["lon"] as? CLLocationDegrees)
+    }
+
+    func testSerializeTrackEvents() {
+        let events = [
+            GeoTrackLocationEvent.startedTracking(message: "fake start"),
+            GeoTrackLocationEvent.error(message: "fake error"),
+            GeoTrackLocationEvent.custom(message: "fake custom")
+        ]
+
+        let track = GeoTrack()
+        events.forEach { track.add(event: $0) }
+        guard let eventMaps = track.map["events"] as? [[String:Any]] else {
+            return XCTFail("Failed to get the right type of data from the map for the events")
+        }
+
+        XCTAssertEqual(3, eventMaps.count)
+        guard let first = eventMaps.first else {
+            return XCTFail("Failed to get the first event from the map array")
+        }
+        XCTAssertEqual(GeoTrackLocationEvent.EventType.startedTrack.rawValue, first["type"] as? Int)
+        XCTAssertNotNil(first["timestamp"])
+        XCTAssertEqual("fake start", first["message"] as? String)
+
+        let second = eventMaps[1]
+        XCTAssertEqual(GeoTrackLocationEvent.EventType.error.rawValue, second["type"] as? Int)
+        XCTAssertNotNil(second["timestamp"])
+        XCTAssertEqual("fake error", second["message"] as? String)
+
+        guard let third = eventMaps.last else {
+            return XCTFail("Failed to get the third event from the map array")
+        }
+        XCTAssertEqual(GeoTrackLocationEvent.EventType.custom.rawValue, third["type"] as? Int)
+        XCTAssertNotNil(third["timestamp"])
+        XCTAssertEqual("fake custom", third["message"] as? String)
+    }
+
+}
+
+// MARK: - Deserialization Tests
+
+extension GeoTrackSerializationTests {
+    func testDeserializeTrackFile() {
+        let reader = TrackReader(filename: "reference-track-1")
+        XCTAssertNotNil(reader)
+        XCTAssertNotNil(reader.track)
+
+        guard let points = reader.track?.points else {
+            return XCTFail("No points")
+        }
+
+        XCTAssertTrue(points.count > 0)
+
+        var last: Date?
+        for point in points {
+            defer {
+                last = point.timestamp
+            }
+            guard let lastTime = last else {
+                continue
+            }
+            XCTAssertTrue(lastTime.timeIntervalSince1970 < point.timestamp.timeIntervalSince1970)
+        }
+    }
+
+    func testValidateDeserializedTrack() {
+        let reader = TrackReader(filename: "reference-track-1")
+        guard let track = reader.track else {
+            return XCTFail("No track")
+        }
+        let analyzer = GeoTrackAnalyzer(track: track)
+        XCTAssertEqual(0, analyzer.legs.count)
+        analyzer.calculate()
+        XCTAssertEqual(12, analyzer.legs.count)
+    }
+}
