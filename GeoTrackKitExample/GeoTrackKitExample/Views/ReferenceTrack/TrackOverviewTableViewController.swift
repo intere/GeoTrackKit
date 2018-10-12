e954fad7fc54480dead9842fd6d36d98454020f9
Eric Internicola <intere@gmail.com>
2018-10-12 17:03:48 -0600

    Incremented the version to 0.4.0, updated the code to swift 4.2 and added a sharing sheet for the Track JSON.

--- GeoTrackKitExample/GeoTrackKitExample/Views/ReferenceTrack/TrackOverviewTableViewController.swift
+++ GeoTrackKitExample/GeoTrackKitExample/Views/ReferenceTrack/TrackOverviewTableViewController.swift
@@ -24,7 +24,7 @@ class TrackOverviewTableViewController: UITableViewController {
     override func viewDidLoad() {
         super.viewDidLoad()
         tableView.estimatedRowHeight = 45
-        tableView.rowHeight = UITableViewAutomaticDimension
+        tableView.rowHeight = UITableView.automaticDimension
     }
 
 }
