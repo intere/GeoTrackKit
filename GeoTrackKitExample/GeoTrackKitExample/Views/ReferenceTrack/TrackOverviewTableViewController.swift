14650765c1f39288a5dbb9b38645faf9063d9f9e
Eric Internicola <intere@gmail.com>
2019-12-19 06:43:46 -0700

    You can now view each leg separately.

--- GeoTrackKitExample/GeoTrackKitExample/Views/ReferenceTrack/TrackOverviewTableViewController.swift
+++ GeoTrackKitExample/GeoTrackKitExample/Views/ReferenceTrack/TrackOverviewTableViewController.swift
@@ -6,6 +6,7 @@
 //  Copyright © 2017 Eric Internicola. All rights reserved.
 //
 
+import CoreLocation
 import GeoTrackKit
 import UIKit
 
@@ -48,7 +49,8 @@ extension TrackOverviewTableViewController {
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        if indexPath.section == 0 {
+        switch indexPath.section {
+        case 0:
             let cell = tableView.dequeueReusableCell(withIdentifier: "TrackOverviewCell", for: indexPath)
             guard let overviewCell = cell as? TrackOverviewCell else {
                 return cell
@@ -56,19 +58,39 @@ extension TrackOverviewTableViewController {
             overviewCell.analyzer = analyzer
 
             return overviewCell
-        }
-        let cell = tableView.dequeueReusableCell(withIdentifier: "LegSwitchCell", for: indexPath)
+        default:
+            let cell = tableView.dequeueReusableCell(withIdentifier: "LegSwitchCell", for: indexPath)
 
-        guard let model = model, let legCell = cell as? LegSwitchCell else {
+            guard let model = model, let legCell = cell as? LegSwitchCell else {
+                return cell
+            }
+
+            legCell.toggleSwitch.isOn = model.isVisible(at: indexPath.row)
+            legCell.label.text = analyzer?.legs[indexPath.row].string
+            legCell.indexPath = indexPath
+            legCell.model = model
+
             return cell
         }
+    }
 
-        legCell.toggleSwitch.isOn = model.isVisible(at: indexPath.row)
-        legCell.label.text = analyzer?.legs[indexPath.row].string
-        legCell.indexPath = indexPath
-        legCell.model = model
+    override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
+        guard indexPath.section != 0, let analyzer = analyzer, indexPath.row < analyzer.legs.count else {
+            return
+        }
+        let leg = analyzer.legs[indexPath.row]
 
-        return cell
+        guard let points = leg.points(from: analyzer.track) else {
+            return
+        }
+
+        let name = "\(leg.isAscent ? "Ascent" : "Descent") #\(indexPath.row / 2 + 1)"
+        let track = GeoTrack(points: points, name: name, description: "")
+
+        let trackMapVC = TrackMapViewController.loadFromStoryboard()
+        trackMapVC.model = UIGeoTrack(with: track)
+        navigationController?.pushViewController(trackMapVC, animated: true)
+        tableView.cellForRow(at: indexPath)?.setSelected(false, animated: true)
     }
 
 }
@@ -76,6 +98,14 @@ extension TrackOverviewTableViewController {
 // MARK: - GeoTrackAnalyzer Helpers
 
 extension Leg {
+
+    var isAscent: Bool {
+        return direction == .upward
+    }
+
+    var isDescent: Bool {
+        return direction == .downward
+    }
 
     var string: String {
         var result = String(index) + " - "
@@ -85,6 +115,16 @@ extension Leg {
         result += String(Int(altitudeChange.metersToFeet)) + "ft"
 
         return result
+    }
+
+    /// Gets you the points for this leg from the track.
+    /// - Parameter track: The track that you want the points for.
+    func points(from track: GeoTrack) -> [CLLocation]? {
+        guard index < track.points.count, endIndex < track.points.count, index < endIndex else {
+            return nil
+        }
+
+        return Array(track.points[index...endIndex])
     }
 
 }
